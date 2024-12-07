d3efba0b742effe13d000942dc671bd13aa7956b
Pratik Patil <Pratikpatil1311@gmail.com>
2024-12-07 14:49:21 +0530

    commited

--- models/Faculty_models/facultySelection.js
+++ models/Faculty_models/facultySelection.js
@@ -5,7 +5,7 @@ const facultySelectionSchema = new mongoose.Schema({
     subject: [{ type: String, required: true }],
     className: { type: String, required: true },
     date: { type: Date, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 3600 } // expires in 1 hour (3600 seconds)
+    createdAt: { type: Date, default: Date.now } // No expiration
 });
 
 const FacultySelection = mongoose.model('FacultySelection', facultySelectionSchema);
